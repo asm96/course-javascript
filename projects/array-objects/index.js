31c56aab63d6a16de2d099ea6d24ec3dd536187f
alex <60008473+alex83d@users.noreply.github.com>
2021-08-02 11:22:52 +0300

    dz

---
+++ projects/array-objects/index.js
@@ -0,0 +1,92 @@
+/* ДЗ 2 - работа с массивами и объектами */
+
+/*
+ Задание 1:
+
+ Напишите аналог встроенного метода forEach для работы с массивами
+ Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
+
+ Пример:
+   forEach([1, 2, 3], (el) => console.log(el))
+ */
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i = i + 1) {
+    fn(array[i], i, array);
+  }
+}
+
+/*
+ Задание 2:
+
+ Напишите аналог встроенного метода map для работы с массивами
+ Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
+
+ Пример:
+   map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
+ */
+function map(array, fn) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray[i] = fn(array[i], i, array);
+  }
+  return newArray;
+}
+
+/*
+ Задание 3:
+
+ Напишите аналог встроенного метода reduce для работы с массивами
+ Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
+
+ Пример:
+   reduce([1, 2, 3], (all, current) => all + current) // 6
+ */
+function reduce(array, fn, initial) {
+  let i,
+    value = initial || array[0];
+  i = !initial ? 1 : 0;
+
+  for (i; i < array.length; i++) {
+    value = fn(value, array[i], i, array);
+  }
+
+  return value;
+}
+/*
+ Задание 4:
+
+ Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистр и вернуть в виде массива
+
+ Пример:
+   upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
+ */
+function upperProps(obj) {
+  const array = [];
+  for (let i in obj) {
+    i = i.toUpperCase();
+    array.push(i);
+  }
+  return array;
+}
+
+/*
+ Задание 5 *:
+
+ Функция принимает объект и должна вернуть Proxy для этого объекта
+ Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+
+ Пример:
+   const obj = createProxy({});
+   obj.foo = 2;
+   console.log(obj.foo); // 4
+ */
+function createProxy(obj) {
+  return new Proxy(obj, {
+    set: function (obj, property, value) {
+      //console.log(`setting '${value}' to '${property}' property`);
+      return (obj[property] = value * value);
+    },
+  });
+}
+
+export { forEach, map, reduce, upperProps, createProxy };
